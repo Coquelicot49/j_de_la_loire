a48deba9358c426227e3b942098c2336c7b78daf
Coquelicot49 <coraline.jadeau@gmail.com>
2023-09-07 11:33:44 +0200

    création du component AG_card. Template d'une carte représentant une AG.

---
+++ client/src/components/ag_card/AG_card.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./AG_card.css"
+import Bouton from '../bouton/Bouton';
+
+const AG_card = () => {
+    return (
+        <div>
+            
+            <div className="cardAG">
+
+                <div className='yearAG'>
+                    1997
+                </div>
+
+                <div className='infoAG'>
+                    Printemps Eté
+                    <br/> Camping
+                    <br/>Longeville sur Mer
+                </div>
+
+                <div className='presentielAG'>
+                    Sociétaires présents :
+                    <ul className='liSocietaire'>
+                        <li>Valérie</li>
+                        <li>Carole</li>
+                        <li>Florence</li>
+                        <li>David</li>
+                        <li>Lucie</li>
+                        <li>Vincent</li>
+                        <li>Charline</li>
+                        <li>Flavie</li>
+                        <li>Coraline</li>
+                    </ul>
+                </div>
+
+                <div className='modifAG'>
+                    <Bouton className="boutonModifAG" texteBouton="✏️"/>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default AG_card;
--- client/src/pages/AssGen.js
+++ client/src/pages/AssGen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../components/header/Header';
+import AG_card from '../components/ag_card/AG_card';
 
 const AssGen = () => {
     return (
         <div>
             <Header/>
             <p>Ceci est la page Jad'or - Assemblée générale</p>
+
+            <AG_card/>
         </div>
     );
 };
