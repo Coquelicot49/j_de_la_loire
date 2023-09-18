f8b65fbba0897205d1fb1ecd95956471b0bb83a5
Coquelicot49 <coraline.jadeau@gmail.com>
2023-09-18 22:19:47 +0200

    fonctionnalité - page AG : ajout filtre par année et par host

--- client/src/pages/ag/AssGen.js
+++ client/src/pages/ag/AssGen.js
@@ -13,28 +13,49 @@ const AssGen = () => {
     // Au besoin, setDataAG prendra des valeurs différentes pour remplir les données dataAG des AG_card
     const [dataAG, setDataAG] = useState(null);
 
+    // Définition de la valeur initial des filtres
+    const [yearFilter, setYearFilter] = useState('')
+    const [hostFilter,setHostFilter] = useState('')
+
+
     // useEffect pour fetcher la table "ag" en totalité via "fetchAG()"
     useEffect(() => {
         fetchAG();
       }, []);
 
     // fonction/requête qui appelle toutes les données de la table "ag"
+    // Et qui ajoute un/des filtre(s) si filtre(s) activé(s)
     const fetchAG = async() => {
         try {
-            let { data: ag, error } = await supabase
+            let query = supabase
             .from('ag')
             .select('*')
             .order('year')
 
-            if (ag) {
-                console.log(ag)
+            if (yearFilter) { query = query.eq('year', yearFilter) }
+            if (hostFilter) { query = query.eq('host', hostFilter) }
+           
+            const {data: ag, error} = await query
                 setDataAG(ag)
                 setTotalAG(ag.length)
-            }
+
         }
         catch (error) {
             console.log(error)
         }
+    }
+
+
+    // ACTIVER LES FILTRES
+    const handleFilter = () => {
+        console.log(yearFilter)
+        fetchAG()
+    }
+
+    // EFFACER LES FILTRES
+    // au click du bouton "Effacer les filtres"
+    const resetFilter = () => { 
+        fetchAG() 
     }
 
     // COMPTABILITSATION NOMBRE AG TOTAL
@@ -59,9 +80,25 @@ const AssGen = () => {
                 <div className='filtreAG'>
                     <div><p> Tu peux filtrer par :</p></div>
                     <div className='filterBox'>
-                        <select><option> par Année </option></select>
-                        <select><option> par type de lieu </option></select>
+                        <select className="filterAGByYear"
+                            onChange={(e) => setYearFilter(e.target.value)}
+                            value={yearFilter}>
+                            <option> par Année </option>
+                            <option> 2008 </option>
+                        </select>
+
+                        <select className="filterAGByYear"
+                            onChange={(e) => setHostFilter(e.target.value)}
+                            value={hostFilter}>
+                            <option> par host </option>
+                            <option> Ski </option></select>
+
                         <select><option> par sociétaire </option></select>
+                    </div>
+
+                    <div id="boutonsFiltres">
+                        <Bouton texteBouton="Filtrer" onClick={handleFilter}/>
+                        <Bouton texteBouton="Effacer les filtres" onClick={resetFilter}/>
                     </div>
 
                     <div id="compteurAG">
@@ -79,7 +116,7 @@ const AssGen = () => {
                     {dataAG ? (
                         //...alors pour chaque ligne de données dispo, rempli une card_AG et affiche là
                         dataAG.map((item) => (
-                            <AG_card year={item.year} season={item.season} place={item.place} host={item.host} id_ag={item.id_ag}/>
+                            <AG_card key={item.id_ag} year={item.year} season={item.season} place={item.place} host={item.host} id_ag={item.id_ag}/>
                         ))
                         //...sinon ou en attendant affiche le message d'attente
                     ) : (<p>En cours de chargement...</p>)}
