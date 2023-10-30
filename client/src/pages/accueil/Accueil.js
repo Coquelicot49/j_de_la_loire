d988b46efa2498ca663c879faa73490c740aa7d6
Coquelicot49 <coraline.jadeau@gmail.com>
2023-10-30 17:55:49 +0100

    ajout de la photo d'accueil

--- client/src/pages/accueil/Accueil.js
+++ client/src/pages/accueil/Accueil.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import "../index.css"
 import Header from '../../components/header/Header';
+import pictureAccueil from "./photoPresentation.png"
+import "./Accueil.css"
 
 const Accueil = () => {
     return (
         <div>
             <Header/>
-            <div className='textContent'>
+            {/* <div className='textContent'>
             <p>C'est la page ACCUEIL</p>
+            </div> */}
+
+            <div className='startPicture'>
+            <img src= {pictureAccueil} alt="photo accueil" />
             </div>
+
         </div>
     );
 };
