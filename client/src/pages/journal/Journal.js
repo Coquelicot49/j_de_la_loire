079619549009049223a9e5dc0bc3b034375c65a0
Coquelicot49 <coraline.jadeau@gmail.com>
2024-11-07 01:37:28 +0100

    ajout des cartes postales de guerre dans la page journal.

--- client/src/pages/journal/Journal.js
+++ client/src/pages/journal/Journal.js
@@ -3,26 +3,32 @@ import Header from '../../components/header/Header';
 import {HashLink as Link} from 'react-router-hash-link';
 import "../index.css"
 import pdfJournal from "../../components/lesPDF/journal_de_guerre.pdf"
+import pdfCartesGuerre from "../../components/lesPDF/cartes_guerre.pdf"
 
 const Journal = () => {
+    
     return (
-        <div>
-            <Header/>
-            <div className='textContent'>
-            <h1>Journal de guerre de Papy Jadeau</h1>
-            <p>Introduction - texte de présentation sur le thème de Papy et la seconde guerre mondiale
-                <br/>et pourquoi pas évoquer des anecdotes sur le père de papy et la première guerre mondiale
-                <br/>auteur(s) à déterminer
-            </p>
-            <p>Accès aux documents :
-                <ul>
-                    <Link to={pdfJournal} target="_blank" rel="noreferrer"><li>Journal de guerre de Papy (version dactylographiée, 23 pages)</li></Link>
-                    <li>Journal de guerre de Papy (version carnet scanné)</li>
-                </ul>
-            </p>
+            <div>
+                <Header/>
+                <div className='textContent'>
+                <h1>Journal de guerre</h1>
+                <p>Introduction - texte de présentation sur le thème de Papy et la seconde guerre mondiale
+                    <br/>et pourquoi pas évoquer des anecdotes sur le père de papy et la première guerre mondiale
+                    <br/>auteur(s) à déterminer
+                </p>
+                <p>Accès au carnet de guerre de Papy en version orginal et dactylographiée :
+                    <ul>
+                        <Link to={pdfJournal} target="_blank" rel="noreferrer"><li>Journal de guerre de Papy (version dactylographiée, 23 pages)</li></Link>
+                        {/* <Link to={pdfCarnet1} target="_blank" rel="noreferrer"><li>Journal de guerre de Papy (version carnet scanné)</li></Link> */}
+                    </ul>
+                </p>
+                <p>Quelques cartes postales envoyées à Mamie par Papi :
+                    <ul>
+                        <Link to={pdfCartesGuerre} target="_blank" rel="noreferrer"><li>Quelques cartes postales envoyées par Papi à Mamie pendant la guerre</li></Link>
+                    </ul>
+                </p>
+                </div>
             </div>
-        </div>
-    );
-};
-
-export default Journal;
+        );
+    };
+    export default Journal;
