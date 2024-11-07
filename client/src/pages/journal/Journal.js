b627c27d9e41a823fd4ac510fa3113d4b2f8581e
Coquelicot49 <coraline.jadeau@gmail.com>
2024-11-07 01:40:17 +0100

    ajout du journal de guerre dans la page journal

--- client/src/pages/journal/Journal.js
+++ client/src/pages/journal/Journal.js
@@ -4,6 +4,7 @@ import {HashLink as Link} from 'react-router-hash-link';
 import "../index.css"
 import pdfJournal from "../../components/lesPDF/journal_de_guerre.pdf"
 import pdfCartesGuerre from "../../components/lesPDF/cartes_guerre.pdf"
+import pdfCarnet1 from "../../components/lesPDF/carnet_papi_1bis.pdf"
 
 const Journal = () => {
     
@@ -19,7 +20,7 @@ const Journal = () => {
                 <p>Accès au carnet de guerre de Papy en version orginal et dactylographiée :
                     <ul>
                         <Link to={pdfJournal} target="_blank" rel="noreferrer"><li>Journal de guerre de Papy (version dactylographiée, 23 pages)</li></Link>
-                        {/* <Link to={pdfCarnet1} target="_blank" rel="noreferrer"><li>Journal de guerre de Papy (version carnet scanné)</li></Link> */}
+                        <Link to={pdfCarnet1} target="_blank" rel="noreferrer"><li>Journal de guerre de Papy (version carnet scanné)</li></Link>
                     </ul>
                 </p>
                 <p>Quelques cartes postales envoyées à Mamie par Papi :
